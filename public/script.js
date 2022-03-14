1c2c53db6f1b5f82ed1f9458129884fa4a40fd82
Moira Leon <moira.leon2@gmail.com>
2022-03-14 16:27:53 -0600

    setting up stream and video

--- public/script.js
+++ public/script.js
@@ -1,7 +1,18 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new PerformanceEntry(undefined, {
-    host: '/'
+    host: '/',
     port: '3001'
+})
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream =>{
+    addVideoStream(myVideo, stream)
 })
 
 myPeer.on('open', id =>{
@@ -11,3 +22,11 @@ myPeer.on('open', id =>{
 socket.on('user-connected', userId =>{
     console.log('User connected: ' + userId)
 })
+
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadmetadata', ()=>{
+        video.play()
+    })
+    videoGrid.append(video)
+}
