14403c79359cec5d0dfcb8d97f2c6334c8690e8b
Moira Leon <moira.leon2@gmail.com>
2022-03-14 16:35:44 -0600

    connecting user stream to our video

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const { connect } = require("mongoose")
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new PerformanceEntry(undefined, {
@@ -13,15 +15,23 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream =>{
     addVideoStream(myVideo, stream)
+
+    socket.on('user-connected', userId =>{
+        connectToNewUser(userId,stream)
+    })
 })
 
 myPeer.on('open', id =>{
     socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.on('user-connected', userId =>{
-    console.log('User connected: ' + userId)
-})
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        addVideoStream(video, userVideoStream)
+    })
+}
 
 function addVideoStream(video, stream){
     video.srcObject = stream
